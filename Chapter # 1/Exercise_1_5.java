4ae01dbd208af3cb7ae14f515170ea54e156647c
Chad-Lister <59707363+Chad-Lister@users.noreply.github.com>
2020-01-21 09:59:41 -0700

    Added Chapter # 1 folder and assignments.

---
+++ Chapter # 1/Exercise_1_5.java
@@ -0,0 +1,11 @@
+/*
+Author:  Chad Lister
+Date: 01/16/2020
+
+This program computes the expression (((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5))
+*/
+class Exercise_1_5 {
+	public static void main(String[] args) {
+		System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+			}
+}
---
+++ Chapter # 1/Exercise_1_9.java
@@ -0,0 +1,12 @@
+/*
+Author:  Chad Lister
+Date: 01/16/2020
+
+This proggram displays the area and perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following formula:  A = W * H
+*/
+class Exercise_1_9 {
+	public static void main(String[] args) {
+		System.out.println("The area of a rectangle with the measurements of width 4.5 and height 7.9 is: "  + (4.5 * 7.9));
+		System.out.println("Its perimeter would be: "  + ((4.5 * 2) + (7.9 * 2)));
+			}
+}
